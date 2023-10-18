1cf17045fe8011d12bfa8f5334a4e64d76bb8ca9
mbares <mbares@fesb.hr>
2023-10-18 13:27:21 +0200

    lab3

---
+++ semester-project/natasa/app/about/page.tsx
@@ -0,0 +1,8 @@
+function About() {
+    return (
+        <main className="flex justify-center text-4xl p-14">
+            <h1>About</h1>
+        </main>
+    );
+}
+export default About;
---
+++ semester-project/natasa/app/showcase/page.tsx
@@ -0,0 +1,8 @@
+function Showcase() {
+    return (
+        <main className="flex justify-center text-4xl p-14">
+            <h1>Showcase</h1>
+        </main>
+    );
+}
+export default Showcase;
