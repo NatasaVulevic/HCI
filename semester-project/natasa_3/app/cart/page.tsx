ea4cbba3f999464512d24555f917912d5ca3e34a
NatasaVulevic <92332547+NatasaVulevic@users.noreply.github.com>
2023-12-07 15:25:37 +0100

    Update page.tsx

--- semester-project/natasa_3/app/cart/page.tsx
+++ semester-project/natasa_3/app/cart/page.tsx
@@ -1,7 +1,7 @@
 function Cart() {
     return (
         <main className="flex justify-center text-4xl p-14">
-            <h1>Cart</h1>
+            <h1>Cart0000000</h1>
         </main>
     );
 }
