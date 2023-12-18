9d812f132312671df56169ff0458322ad8b5a95b
NatasaVulevic <natasa.vulevic00@fesb.hr>
2023-12-18 18:37:27 +0100

    add

--- semester-project/natasa_3/app/page.tsx
+++ semester-project/natasa_3/app/page.tsx
@@ -3,7 +3,7 @@ import slika1 from "/public/slika4.png";
 import slika2 from "/public/new-arrival.png";
 import slika3 from "/public/professional-use.png";
 import Link from "next/link";
-
+//flex flex-col justify-center
 
 export default function Home() {
 
@@ -34,11 +34,11 @@ export default function Home() {
 
       </div>
 
-      <div className=" 	my-36	 grid sm:grid-cols-1 md:grid-cols-2 md:h-96 overflow-hidden md:scale-110">   
+      <div className=" 	my-36	 grid sm:grid-cols-1 md:grid-cols-2 md:h-96 overflow-hidden md:scale-y-110">   
            {/*  <Link href="" className="h-72 md:h-full"> */}
               <div className="bg-gradient-to-b from-gray-400 to-gray-100 h-80 md:h-96 ">     
               <Link  href="">  
-                    <div className="  mt-24 md:mt-32 text-center font-tharlon  hover:scale-105">
+                    <div className="md:scale-y-90 items-center text-center mt-24 md:mt-32  font-tharlon  hover:scale-105">
                           <div className=" text-xl md:text-2xl font-medium m-8 md:m-10 ">
                             NEW ARRIVALS
                           </div>
@@ -55,7 +55,7 @@ export default function Home() {
       
       <div className="my-24 " >
         <div className="font-playfair text-2xl font-medium italic mb-8 ms-6 text-gray-800">
-          COLLECTION
+          COLLECTIONS
         </div>    
 
         <div className="grid	place-items-center mx-4 gap-2 overflow-hidden sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
@@ -83,16 +83,12 @@ export default function Home() {
           </div>       
       </div>
 
-    <div className=" grid sm:grid-cols-1 md:grid-cols-2 my-36 w-full md:h-1/2 md:scale-110" >
+    <div className=" grid sm:grid-cols-1 md:grid-cols-2 my-36 w-full md:h-1/2 md:scale-y-110 " >
+    
+           <div className="bg-gradient-to-b from-gray-400 to-gray-100 h-80 md:h-full overflow:hidden  ">   
 
-      {/*   <div className="invisible md:visible" >
-            <Image className="h-80 md:h-96 w-full " src={slika3}  alt="Professional Use"/>          
-        </div> */}
-
-        <div className="bg-gradient-to-b from-gray-400 to-gray-100 h-80 md:h-full overflow:hidden  ">   
-
-       <Link href= "" >     <div className=" mt-24 md:mt-32 text-center font-tharlon hover:scale-105  ">
-                <div className=" text-xl md:text-2xl font-medium m-8 md:m-10">
+       <Link href= "" >     <div className=" mt-24 md:mt-32 text-center font-tharlon hover:scale-105 md:scale-y-90 ">
+                <div className=" text-xl md:text-2xl font-medium m-8 md:m-10 ">
                   PROFESSIONAL USE
                 </div>
                 <div className="text-l md:text-xl">
@@ -102,8 +98,8 @@ export default function Home() {
               </div> </Link> 
         </div> 
 
-        <div /* className="visible md:invisible" */ >
-            <Image className="h-80 md:h-96 w-full " src={slika3}  alt="Professional Use"/>           {/* "w-full h-80 md:h-96" */}
+        <div>
+            <Image className="h-80 md:h-96 w-full " src={slika3}  alt="Professional Use"/>          
         </div>
       
     </div>
